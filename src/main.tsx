a538d4bde71c3bbeca1d25482e990704fc345e52
jmmccota <joao_marcos_cota@hotmail.com>
2025-02-04 16:07:39 -0300

    SEO Improvements

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./normalize.css";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
 function AppWithRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<App />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
   );
